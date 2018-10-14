6ea58f0334dfac6c15efb844636207d4f51a4db0
Johnny Ruiz <jeko2000@yandex.com>
2018-10-14 14:42:05 -0400

    Customize default development environment packages

--- chroot-install.sh
+++ chroot-install.sh
@@ -57,12 +57,11 @@ echo 'vboxsf' > /etc/modules-load.d/vboxsf.conf
 
 # install dev envt.
 echo 'Installing dev environment'
-pacman -S --noconfirm git emacs zsh nodejs npm vim wget perl make gcc grep tmux i3 dmenu
+pacman -S --noconfirm git emacs zsh nodejs npm vim wget perl make gcc grep tmux i3 dmenu w3m the_silver_searcher aspell aspell-en aspell-es
 pacman -S --noconfirm chromium curl autojump openssh sudo mlocate the_silver_searcher
 pacman -S --noconfirm ttf-hack lxterminal nitrogen ntp dhclient keychain
-pacman -S --noconfirm python-pip go go-tools pkg-config
+pacman -S --noconfirm python-pip go go-tools pkg-config python-pipenv ipython python-requests yapf python-jedi
 npm install -g jscs jshint bower grunt
-pip install pipenv bpython ipython
 
 # install req for pacaur & cower
 echo 'Installing dependencies'
